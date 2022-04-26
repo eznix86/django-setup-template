e722902b493da3abea24457eeab860630d1e8480
Bernard Joseph Jean Bruno <brunobernard@busymind101.com>
2022-04-26 18:49:17 +0400

    env

--- install.sh
+++ install.sh
@@ -2,9 +2,13 @@
 
 set -euo pipefail
 
+CORE_SETTINGS="./core/settings"
+TEMPLATE_FOLDER="./templates"
+PROJECT_NAME="django-setup-template"
+
 function init() {
-  test -d "./django-setup-template" || git clone https://github.com/theArtechnology/django-setup-template.git
-  cp -R django-setup-template/* .
+  test -d "./django-setup-template" || git clone https://github.com/theArtechnology/${PROJECT_NAME}.git
+  cp -R ${PROJECT_NAME}/* .
 }
 
 function setup_python() {
@@ -17,25 +21,25 @@ function setup_python() {
 
 function setup_core_project() {
     test -d core || django-admin startproject core .
-    mkdir ./core/settings
-    touch ./core/settings/{__init__.py,defaults.py,prod.py,dev.py,staging.py}
-    awk 'NR==16 {$0="BASE_DIR = Path(__file__).resolve().parent.parent.parent"} { print }' ./core/settings.py >  ./core/settings/defaults.py 
+    mkdir ${CORE_SETTINGS}
+    touch ${CORE_SETTINGS}/{__init__.py,defaults.py,prod.py,dev.py,staging.py}
+    awk 'NR==16 {$0="BASE_DIR = Path(__file__).resolve().parent.parent.parent"} { print }' ${CORE_SETTINGS}.py >  ${CORE_SETTINGS}/defaults.py 
 }
 
 
 function create_files_from_templates() {
-    cat templates/README.md > README.md
-    cat templates/init.py > ./core/settings/__init__.py
-    cat templates/dev.py > ./core/settings/dev.py
-    cat templates/prod.py > ./core/settings/prod.py
-    cat templates/staging.py > ./core/settings/staging.py
+    cat ${TEMPLATE_FOLDER}/README.md > README.md
+    cat ${TEMPLATE_FOLDER}/init.py > ${CORE_SETTINGS}/__init__.py
+    cat ${TEMPLATE_FOLDER}/dev.py > ${CORE_SETTINGS}/dev.py
+    cat ${TEMPLATE_FOLDER}/prod.py > ${CORE_SETTINGS}/prod.py
+    cat ${TEMPLATE_FOLDER}/staging.py > ${CORE_SETTINGS}/staging.py
 }
 
 function clean_up() {
-    rm ./core/settings.py
-    rm -rf django-setup-template
+    rm ${CORE_SETTINGS}.py
+    rm -rf ${PROJECT_NAME}
     rm install.sh
-    rm -rf templates
+    rm -rf ${TEMPLATE_FOLDER}
 }
 
 
